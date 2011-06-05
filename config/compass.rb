772bca20b94792513f86d508a16ffafa4237d40b
James N. V. Cash <james.nvc@gmail.com>
2011-06-04 20:19:11 -0400

    Initial commit

---
+++ config/compass.rb
@@ -0,0 +1,7 @@
+project_type = :rails
+project_path = Compass::AppIntegration::Rails.root
+environment  = Compass::AppIntegration::Rails.env
+
+http_path = '/'
+css_dir   = 'tmp/stylesheets'
+sass_dir  = 'app/views/stylesheets'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerAcademy::Application.config.secret_token = '47bb81487db2b817159b38e6a71bde91ac25c0c4c05b18cbe15fedfcf1d8c321eb3d98a45109ed4d9a6ad37aa54eba644c0970eb194e9877c282ce550dda74f5'
