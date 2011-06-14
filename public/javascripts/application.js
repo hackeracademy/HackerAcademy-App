b3c5630f0cecb976b25c4aac4d66fafa7d707dba
James N. V. Cash <james.nvc@gmail.com>
2011-06-14 07:50:12 -0400

    Initial baked javascript

---
+++ public/javascripts/application.js
@@ -0,0 +1,11 @@
+/* DO NOT MODIFY. This file was compiled Tue, 14 Jun 2011 11:49:41 GMT from
+ * /Users/james/Programming/projects/HackerAcademy/app/scripts/application.coffee
+ */
+
+(function() {
+  var powers;
+  powers = [1, 2, 3, 4].map(function(i) {
+    return i * i;
+  });
+  console.info(powers);
+}).call(this);
