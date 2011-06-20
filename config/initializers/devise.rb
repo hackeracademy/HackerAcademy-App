64e7df68c19ec9e2361b59659eab832e08bff271
James N. V. Cash <james.nvc@gmail.com>
2011-06-19 20:02:20 -0400

    Change email address for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "webmaster@skule.ca"
+  config.mailer_sender = "info@hackeracademy.org"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
