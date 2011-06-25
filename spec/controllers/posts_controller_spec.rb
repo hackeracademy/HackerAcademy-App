8f0fd816934b42c849ee6ec04f5ea69d5d9c343f
James N. V. Cash <james.nvc@gmail.com>
2011-06-25 09:16:18 -0400

    Updating posts tests to include authentication

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -24,7 +24,17 @@ describe PostsController do
   # Post. As you add validations to Post, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {
+      :title => "Test Post",
+      :body => "Lorem ipsum delores est"
+    }
+  end
+  def get_user
+    return User.create!({
+      :name => "tester",
+      :password => "testing",
+      :email => "test@testing.com"
+    })
   end
 
   describe "GET index" do
@@ -44,14 +54,29 @@ describe PostsController do
   end
 
   describe "GET new" do
+    it "should fail when not logged in" do
+      get :new
+      assigns(:post).should be_nil
+      response.should redirect_to(new_user_session_path)
+    end
+
     it "assigns a new post as @post" do
+      sign_in get_user
       get :new
       assigns(:post).should be_a_new(Post)
     end
   end
 
   describe "GET edit" do
+    it "should fail when not logged in" do
+      post = Post.create! valid_attributes
+      get :edit, :id => post.id.to_s
+      assigns(:post).should be_nil
+      response.should redirect_to(new_user_session_path)
+    end
+
     it "assigns the requested post as @post" do
+      sign_in get_user
       post = Post.create! valid_attributes
       get :edit, :id => post.id.to_s
       assigns(:post).should eq(post)
@@ -60,19 +85,28 @@ describe PostsController do
 
   describe "POST create" do
     describe "with valid params" do
+      it "should fail when not logged in" do
+        post :create, :post => valid_attributes
+        assigns(:post).should be_nil
+        response.should redirect_to(new_user_session_path)
+      end
+
       it "creates a new Post" do
+        sign_in get_user
         expect {
           post :create, :post => valid_attributes
         }.to change(Post, :count).by(1)
       end
 
       it "assigns a newly created post as @post" do
+        sign_in get_user
         post :create, :post => valid_attributes
         assigns(:post).should be_a(Post)
         assigns(:post).should be_persisted
       end
 
       it "redirects to the created post" do
+        sign_in get_user
         post :create, :post => valid_attributes
         response.should redirect_to(Post.last)
       end
@@ -80,6 +114,7 @@ describe PostsController do
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved post as @post" do
+        sign_in get_user
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
         post :create, :post => {}
@@ -87,6 +122,7 @@ describe PostsController do
       end
 
       it "re-renders the 'new' template" do
+        sign_in get_user
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
         post :create, :post => {}
@@ -97,23 +133,32 @@ describe PostsController do
 
   describe "PUT update" do
     describe "with valid params" do
+      it "should fail when not logged in" do
+        post = Post.create! valid_attributes
+        put :update, :id => post.id, :post => {:title => "New Title"}
+        response.should redirect_to(new_user_session_path)
+      end
+
       it "updates the requested post" do
         post = Post.create! valid_attributes
         # Assuming there are no other posts in the database, this
         # specifies that the Post created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
-        Post.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => post.id, :post => {'these' => 'params'}
+        Post.any_instance.should_receive(:update_attributes).with({'title' => "New Title"})
+        sign_in get_user
+        put :update, :id => post.id, :post => {'title' => "New Title"}
       end
 
       it "assigns the requested post as @post" do
+        sign_in get_user
         post = Post.create! valid_attributes
         put :update, :id => post.id, :post => valid_attributes
         assigns(:post).should eq(post)
       end
 
       it "redirects to the post" do
+        sign_in get_user
         post = Post.create! valid_attributes
         put :update, :id => post.id, :post => valid_attributes
         response.should redirect_to(post)
@@ -122,6 +167,7 @@ describe PostsController do
 
     describe "with invalid params" do
       it "assigns the post as @post" do
+        sign_in get_user
         post = Post.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
@@ -130,6 +176,7 @@ describe PostsController do
       end
 
       it "re-renders the 'edit' template" do
+        sign_in get_user
         post = Post.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
@@ -140,7 +187,16 @@ describe PostsController do
   end
 
   describe "DELETE destroy" do
+    it "should fail when not logged in" do
+      post = Post.create! valid_attributes
+      expect {
+        delete :destroy, :id => post.id.to_s
+      }.to change(Post, :count).by(0)
+      response.should redirect_to(new_user_session_path)
+    end
+
     it "destroys the requested post" do
+      sign_in get_user
       post = Post.create! valid_attributes
       expect {
         delete :destroy, :id => post.id.to_s
@@ -148,6 +204,7 @@ describe PostsController do
     end
 
     it "redirects to the posts list" do
+      sign_in get_user
       post = Post.create! valid_attributes
       delete :destroy, :id => post.id.to_s
       response.should redirect_to(posts_url)
