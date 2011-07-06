fe46e675c74992dfaa1e32b8340a0f05ca23927e
James N. V. Cash <james.nvc@gmail.com>
2011-07-06 17:24:07 -0400

    Working on contests

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,6 +1,8 @@
 class Contest
   include Mongoid::Document
+  include Mongoid::MultiParameterAttributes
   field :description, :type => String
-  field :start, :type => Date
-  field :end, :type => Date
+  field :problem, :type => String
+  field :start, :type => DateTime
+  field :end, :type => DateTime
 end
