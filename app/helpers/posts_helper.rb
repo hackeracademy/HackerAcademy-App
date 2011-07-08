c43e1d441010dbdcf6fb28890bc080e4dda5d021
James N. V. Cash <james.nvc@gmail.com>
2011-07-08 18:44:28 -0400

    Sanitize markdown-generated HTML

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def markdown(text)
     options = [:filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-    Redcarpet.new(text, *options).to_html
+    Redcarpet.new(text, *options).to_html.html_safe
   end
 end
