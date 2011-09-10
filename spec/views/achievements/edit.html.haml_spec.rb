a68cf7842301c91a20e51542b665916dc91ecc6e
James N. V. Cash <james.nvc@gmail.com>
2011-09-10 18:41:34 -0400

    Fixing achivement view specs

--- spec/views/achievements/edit.html.haml_spec.rb
+++ spec/views/achievements/edit.html.haml_spec.rb
@@ -4,7 +4,8 @@ describe "achievements/edit.html.haml" do
   before(:each) do
     @achievement = assign(:achievement, stub_model(Achievement,
       :name => "MyString",
-      :description => "MyString"
+      :description => "MyString",
+      :value => 10
     ))
   end
 
@@ -14,7 +15,8 @@ describe "achievements/edit.html.haml" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => achievements_path(@achievement), :method => "post" do
       assert_select "input#achievement_name", :name => "achievement[name]"
-      assert_select "input#achievement_description", :name => "achievement[description]"
+      assert_select "input#achievement_value", :name => "achievement[value]"
+      assert_select "textarea#achievement_description", :name => "achievement[description]"
     end
   end
 end
