99efefb9ee6cddbc9bb1483cce5c161aede6b061
James N. V. Cash <james.nvc@gmail.com>
2011-09-27 18:51:32 -0400

    New code for the next achievement

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,14 +5,15 @@ class PageController < ApplicationController
     render params[:name]
   end
 
+  # TODO: Fix this super-hacky solution to something that actually scales...
   def redeem
     if current_user.nil?
       redirect_to new_user_session_path, alert: 'You need to log in first'
       return
     end
-    first_cheevo = Achievement.first(conditions: {name: 'First Meeting!'})
-    if params[:code].downcase.gsub(/\s+/, '') == 'purplellama'
-      current_user.achievements << first_cheevo
+    cheevo = Achievement.first(conditions: {name: 'Second Meeting!'})
+    if params[:code].downcase.gsub(/\s+/, '') == 'skytaffy'
+      current_user.achievements << cheevo
     else
       redirect_to "/redeem", alert: 'Sorry, invalid code :('
       return
