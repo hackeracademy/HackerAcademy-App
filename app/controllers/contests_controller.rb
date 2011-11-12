fa3a295d78b864f607b4330d25576ca85304014a
Rafal Dittwald <rafal.dittwald@gmail.com>
2011-11-12 16:53:42 -0500

    disable email

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -184,15 +184,17 @@ class ContestsController < ApplicationController
     if correct
 
       if ENV["RAILS_ENV"] == "production"
-        Pony.mail(
-          :to => 'rafal.dittwald@gmail.com', :cc => 'james.nvc@gmail.com',
-          :from => 'dojobot@hackeracademy.org',
-          :subject => "#{current_user.name} has solved problem #{level} at #{Time.now}")
-        if perf != -1
-           Pony.mail(
-          :to => 'rafal.dittwald@gmail.com', :cc => 'james.nvc@gmail.com',
-          :from => 'dojobot@hackeracademy.org',
-          :subject => "#{perf.round(2)} #{current_user.name} P#{level} at #{Time.now}")
+        if false
+          Pony.mail(
+            :to => 'rafal.dittwald@gmail.com', :cc => 'james.nvc@gmail.com',
+            :from => 'dojobot@hackeracademy.org',
+            :subject => "#{current_user.name} has solved problem #{level} at #{Time.now}")
+          if perf != -1
+             Pony.mail(
+            :to => 'rafal.dittwald@gmail.com', :cc => 'james.nvc@gmail.com',
+            :from => 'dojobot@hackeracademy.org',
+            :subject => "#{perf.round(2)} #{current_user.name} P#{level} at #{Time.now}")
+          end
         end
       end
       redirect_to contest, notice: 'Congratulations! Your solution was correct!'
@@ -204,10 +206,12 @@ class ContestsController < ApplicationController
 
       if perf != -1
         if ENV["RAILS_ENV"] == "production"
+        if false
         Pony.mail(
           :to => 'rafal.dittwald@gmail.com', :cc => 'james.nvc@gmail.com',
           :from => 'dojobot@hackeracademy.org',
           :subject => "#{perf.round(2)} #{current_user.name} P#{level} at #{Time.now}")
+        end
         end
         redirect_to contest, alert: "Sorry, your solution was not good enough. P=#{perf.round(2)}"
       else
